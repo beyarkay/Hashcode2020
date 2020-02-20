810cad631d6b71f3ec7a5bf7492e79bd16184297
Luc Hayward <luchay10@gmail.com>
2020-02-20 19:56:16 +0200

    Book input

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book{
+	public int score;
+	
+	public Book(){
+	
+	}
+	public Book(int score){
+		this.score = score;
+	}
+}
--- src/Scratchpad.java
+++ src/Scratchpad.java
@@ -1,18 +1,32 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Scratchpad {
 	
-	static int m, n;
+	static int numBooks, numLibraries, numDays;
+	static List<Book> bookList;
+	static List<Library> libraryList;
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		Scanner scanner = new Scanner(new FileReader("small.in"));
-		m = scanner.nextInt();
-		n = scanner.nextInt();
+		numBooks = scanner.nextInt(); // B
+		numLibraries = scanner.nextInt(); // L
+		numDays = scanner.nextInt(); // D
+		scanner.nextLine();
+		
+		for (int i = 0; i < numBooks; i++) {
+			bookList.add(new Book(scanner.nextInt()));
+		}
+		scanner.nextLine();
+		
+		
 		
 		scanner.close();
 	}
 //
 }
+
