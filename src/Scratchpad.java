a945f80bcb83225aa945a353d48b31cba676fa7d
beyarkay <boydrkane@gmail.com>
2020-02-20 21:00:05 +0200

    MVP complete

--- src/Scratchpad.java
+++ src/Scratchpad.java
@@ -1,9 +1,5 @@
 import java.io.*;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.ArrayList;
+import java.util.*;
 
 
 public class Scratchpad {
@@ -16,31 +12,31 @@ public class Scratchpad {
 			"e_so_many_books.txt",
 			"f_libraries_of_the_world.txt"
 	};
-	
+
 	static int numBooks, numLibraries, numDays;
 	static List<Book> bookList;
 	static List<Library> libraryList;
 	//	static List<Library> signedUpLibraries;
 	static SortedSet<Library> signedUpLibraries;
-	
+
 	static String outFile = "output.txt";
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
-		
+
 		Scanner scanner = new Scanner(new FileReader(INPUT_FILES[0]));
 		numBooks = scanner.nextInt(); // B
 		numLibraries = scanner.nextInt(); // L
 		numDays = scanner.nextInt(); // D
 		scanner.nextLine();
-		
+
 		bookList = new ArrayList<>(numBooks);
 		libraryList = new ArrayList<>(numLibraries);
-		
+
 		for (int i = 0; i < numBooks; i++) {
 			bookList.add(new Book(scanner.nextInt(), i));
 		}
 		scanner.nextLine();
-		
+
 		for (int i = 0; i < numLibraries; i++) {
 			int Nj = scanner.nextInt();
 			libraryList.add(new Library(Nj, scanner.nextInt(), scanner.nextInt(), i));
@@ -49,23 +45,38 @@ public class Scratchpad {
 				libraryList.get(i).booksInLibrary.add(bookList.get(scanner.nextInt()));
 			}
 		}
-		
+
 		scanner.close();
+
+
+		MVP_boyd();
+		System.out.println(signedUpLibraries.size());
+		PrintOutput();
 	}
 
-	public static void MVP_boyd(){
+	public static void MVP_boyd() {
+		Comparator<Library> libraryComparator = Comparator.comparingInt(l -> l.numBooks);
+		signedUpLibraries = new TreeSet<>(libraryComparator);
 
+		Comparator<Book> bookComparator = Comparator.comparingInt(b -> b.score);
 
-
+		for (Library l : libraryList) {
+			for(Book b: l.booksInLibrary){
+				l.scannedBooks = new TreeSet<>(bookComparator);
+				l.scannedBooks.add(b);
+			}
+			signedUpLibraries.add(l);
+		}
 	}
 
-	
+
 	public static void PrintOutput() {
 		try {
 			PrintWriter pr = new PrintWriter(new FileWriter(new File(outFile)));
-			
+
 			pr.println(signedUpLibraries.size());
 			for (Library library : signedUpLibraries) {
+
 				pr.print(library.id);
 				pr.print(" ");
 				pr.print(library.scannedBooks.size());
@@ -75,13 +86,10 @@ public class Scratchpad {
 				}
 				pr.print("\b");
 			}
-			
-			
-		}
-		catch (IOException e) {
+			pr.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 //
 }
